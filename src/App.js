b1fdcdaf8d45db8a4128c75d46277ff3cfde690b
JvanRA <anorajivan@gmail.com>
2022-08-06 14:57:39 +0800

    added print button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles/Style.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import SideContent from './components/SideContent';
+import ReactToPrint from 'react-to-print';
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <MainContent />
-      <SideContent />
-    </div>
-  )
+
+class App extends Component {
+  render() {
+    return(
+      <div className='app-container'>
+        <ReactToPrint
+          id='print-btn'
+          trigger={() => {
+            return <button>Print</button>
+          }}
+          content={() => this.componentRef}
+        />
+        <div className='App' ref={el=>(this.componentRef = el)}>
+          <Header />
+          <MainContent />
+          <SideContent />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
